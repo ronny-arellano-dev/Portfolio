be84ea74227822ee2000562acc1a1fb9c931c7d2
RedW1dow <ronnyarellano11@gmail.com>
2022-08-01 13:32:08 -0700

    Initial Commit of charSheet with needed classes.

---
+++ Java/DnDCharacterGenerator/src/com/redwidow/charactergenerator/charSheet.java
@@ -0,0 +1,21 @@
+package com.redwidow.charactergenerator;
+
+public class charSheet {
+    // Saving Throws
+    public class savingThrows {
+        int stSTR,stDEX,stCON,stINT,stWIS,stCHA;
+    }
+
+    // Skills
+    public class skills {
+        int acrobatics,animalHandling,arcana,athletics,deception,history,
+                intimidation,investigation,medicine,perception,performance,
+                persuasion,religion,sleightOfHand,survival;
+
+    }
+
+    // HP & AC
+    public class hpAC {
+        int armorClass,initiative,speed,hitDice,maxHP,proficiencyBonus;
+    }
+}

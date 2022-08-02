af510ed7856a5179b3e18d8a7ccf2583ef35338a
RedW1dow <ronnyarellano11@gmail.com>
2022-08-01 17:43:48 -0700

    updates

---
+++ Java/DnDCharacterGenerator/src/com/redwidow/charactergenerator/hpAC.java
@@ -0,0 +1,13 @@
+package com.redwidow.charactergenerator;
+
+public class hpAC {  
+
+    // HP & AC
+    static int[] getHPAC() {
+        int armorClass,initiative,speed,hitDice,maxHP,proficiencyBonus;
+        int[] setHPAC = new int[6];
+
+        return setHPAC;
+    }
+    
+}
